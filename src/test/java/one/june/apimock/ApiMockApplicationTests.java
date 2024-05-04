5e6bc716e395f0a4143348d41b88ab0aab111a5f
Manish Kumar Lath <manishlath3445@gmail.com>
2024-05-05 02:34:37 +0530

    Initial Commit

---
+++ src/test/java/one/june/apimock/ApiMockApplicationTests.java
@@ -0,0 +1,13 @@
+package one.june.apimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
