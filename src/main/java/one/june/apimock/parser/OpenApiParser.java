1f02e2907b480e6bf5e93f77da8e979b10c785b8
Manish Kumar Lath <manishlath3445@gmail.com>
2024-05-18 02:20:55 +0530

    Add logs in parse openapi doc flow

--- src/main/java/one/june/apimock/parser/OpenApiParser.java
+++ src/main/java/one/june/apimock/parser/OpenApiParser.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.parser.OpenAPIV3Parser;
+import lombok.extern.slf4j.Slf4j;
 import one.june.apimock.model.MockRequest;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 public class OpenApiParser {
     private final List<SchemaExtractor> extractors = List.of(
             new GetSchemaExtractor(),
@@ -23,13 +25,12 @@ public class OpenApiParser {
         Paths paths = openAPI.getPaths();
 
         ArrayList<MockRequest> mockRequests = new ArrayList<>();
-        paths.forEach((String path, PathItem pathItem) -> {
-            extractors.forEach(extractor -> {
-                MockRequest mockRequest = extractor.extract(path, pathItem);
-                if (mockRequest != null) mockRequests.add(mockRequest);
-            });
-        });
+        paths.forEach((String path, PathItem pathItem) -> extractors.forEach(extractor -> {
+            MockRequest mockRequest = extractor.extract(path, pathItem);
+            if (mockRequest != null) mockRequests.add(mockRequest);
+        }));
 
+        log.info("Found {} APIs to be mocked for file {} ", mockRequests.size(), filePath);
         return mockRequests;
     }
 }
--- src/main/java/one/june/apimock/service/MockService.java
+++ src/main/java/one/june/apimock/service/MockService.java
@@ -1,6 +1,7 @@
 package one.june.apimock.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import one.june.apimock.model.MockRequest;
 import one.june.apimock.parser.OpenApiParser;
 import org.springframework.stereotype.Service;
@@ -9,20 +10,29 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class MockService {
     private final OpenApiParser parser;
 
     public List<MockRequest> parseOpenApi(MultipartFile openApiDoc) {
+        String filename = openApiDoc.getOriginalFilename();
         try {
-            String extension = openApiDoc.getOriginalFilename().substring(openApiDoc.getOriginalFilename().lastIndexOf('.'));
+            String extension = Objects.requireNonNull(filename).substring(filename.lastIndexOf('.'));
             File file = File.createTempFile("openApi", extension);
             openApiDoc.transferTo(file);
 
-            return parser.parse(file.getAbsolutePath());
+            String filePath = file.getAbsolutePath();
+            log.info("Received file {} for parsing with path {}.", filename, filePath);
+            return parser.parse(filePath);
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            log.error("Invalid file: {} due to {} ", filename, e.getMessage(), e);
+            throw new RuntimeException(e);
         } catch (IOException e) {
+            log.error("Error parsing file {} due to {} ", filename, e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
